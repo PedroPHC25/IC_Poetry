408a6b5f20c9abec113867ad9e130c34e1b37015
PEDRO HENRIQUE COTERLI <B51063@fgv.edu.br>
2023-04-26 09:04:21 -0300

    Aula 26/04

--- Relatório/Cria.py
+++ Relatório/Cria.py
@@ -1,10 +1,14 @@
+import os
+
 class Relatório:
-    def __init__(self, título, autor):
+    template_dir = os.path.join('..', 'dados')
+    def __init__(self, título, autor, template = 'cabeçalho.tex'):
+        self.template = template
         self.cabeçalho = None
         self.título = título
         self.autor = autor
         self.inicializa()
         
     def inicializa(self):
-        with open('..dados/cabeçalho.tex') as f:
+        with open(os.path.join(self.template_dir, 'cabeçalho.tex')) as f:
             self.cabeçalho = f.read()
