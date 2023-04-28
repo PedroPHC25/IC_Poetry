99e5024b4e89f06075f7406feb732e0793021aac
PEDRO HENRIQUE COTERLI <B51063@fgv.edu.br>
2023-04-28 09:02:34 -0300

    Aula 28/04

--- Relatório/Cria.py
+++ Relatório/Cria.py
@@ -1,14 +1,19 @@
+from jinja2 import Environment, PackageLoader
 import os
 
 class Relatório:
     template_dir = 'dados'
     def __init__(self, título, autor, template = 'cabeçalho.tex'):
-        self.template = template
+        env = Environment(loader = PackageLoader('Poetry'))
+        self.template = env.get_template(self.template_file)
         self.cabeçalho = None
         self.título = título
         self.autor = autor
-        self.inicializa()
+        self.sections = []
+        self.context = {'autor': self.autor, 'título': self.título, 'sections': self.sections}
         
-    def inicializa(self):
-        with open(os.path.join(self.template_dir, 'cabeçalho.tex')) as f:
-            self.cabeçalho = f.read()
+    def render(self):
+        return self.template.render(self.context)
+    
+    def add_section(self, section):
+        self.sections.append(section)
--- tests/relatorio/test_cria.py
+++ tests/relatorio/test_cria.py
@@ -1,7 +1,19 @@
 import unittest
-from Relatório.Cria import Relatorio
+from Relatório.cria import Relatorio
 
 class TestCria(unittest.TestCase):
     def test_cria_relatorio(self):
         R = Relatorio('Meu relatório', 'Eu mesmo')
         self.assertEqual(R.titulo, 'Meu relatório')
+    
+    def test_render(self):
+        R = Relatorio('Meu relatório', 'Eu mesmo')
+        tex = R.render()
+        self.assertIn('\\author{ Eu mesmo }', tex)
+        self.assertIn('\\title{ Meu relatório }', tex)
+
+    def test_add_section(self):
+        R = Relatorio('Meu relatório', 'Eu mesmo')
+        R.add_section('\section{primeira seção}')
+        tex = R.render()
+        self.assertIn('\section{primeira seção}', tex)
